4681a943a64a78fc49b2ff7c9852267cb780a8fc
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-10-04 17:47:52 -0500

    added models and tables

---
+++ db/migrate/20211004214942_create_cars.rb
@@ -0,0 +1,13 @@
+class CreateCars < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cars do |t|
+      t.string :make
+      t.string :model
+      t.string :img_url
+      t.string :year
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
