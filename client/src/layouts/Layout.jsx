c3dcede0faf0eb36cc5aaa565084210f4dd0f3ac
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-10-06 13:13:57 -0500

    Made something actuall render on screen

---
+++ client/src/layouts/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+    <div>
+      <header>
+        <h1>APB</h1>
+        <Link>Login/Register</Link>
+
+      </header>
+    </div>
+  )
+}
