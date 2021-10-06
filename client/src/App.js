046b98e0997e99f664e24ca850cb836e2c2c8d41
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-10-06 13:37:49 -0500

    Layout and Login page are running

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router';
 import Layout from './layouts/Layout';
+import Login from './screens/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <Layout>
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+        </Switch>
+
+      </Layout >
 
     </div>
   );
--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Layout() {
+export default function Layout(props) {
   return (
     <div>
       <header>
         <h1>APB</h1>
-        <Link>Login/Register</Link>
-
+        <Link to="/login">Login/Register</Link>
+        <hr />
       </header>
+      {props.children}
     </div>
   )
 }
