ef2f3208b7311c86be9670ffcb4ba1329abfd252
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-10-06 14:31:28 -0500

    login successfully made

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import { Switch, Route } from 'react-router';
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router';
 import Layout from './layouts/Layout';
 import Login from './screens/Login/Login';
-
+import {
+  loginUser,
+  registerUser,
+  verifyUser,
+  removeToken,
+} from './services/auth';
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+  const history = useHistory();
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+
+
   return (
     <div className="App">
       <Layout>
         <Switch>
           <Route path='/login'>
-            <Login />
+            <Login handleLogin={handleLogin} />
           </Route>
         </Switch>
 
