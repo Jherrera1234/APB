b3707cdb33151af1cd067c8f020ae03e8f66cbbb
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-10-06 19:09:38 -0500

    cars screens works

--- client/src/containers/maincontainer/MainContainer.jsx
+++ client/src/containers/maincontainer/MainContainer.jsx
@@ -1,19 +1,31 @@
 import { useState, useEffect } from 'react';
 import { Switch, Route, useHistory } from 'react-router-dom';
-import { getAllCars, getOneCar, postCar, deleteCar, putCar } from '../services/cars';
-import Cars from '../screens/Cars';
-import CarCreate from '../screens/CarCreate';
-import CarEdit from '../screens/CarEdit';
-import CarDetail from '../screens/CarDetail';
+import { getAllCars, getOneCar, postCar, deleteCar, putCar } from '../../services/cars';
+import Cars from '../../screens/Cars/Cars';
+// import CarCreate from '../../screens/CarCreate';
+// import CarEdit from '../../screens/CarEdit';
+// import CarDetail from '../../screens/CarDetail';
 export default function MainContainer() {
   const [cars, setCars] = useState([]);
   const [categories, setCategories] = useState([]);
   const [parts, setParts] = useState([]);
   const history = useHistory();
 
+
+  useEffect(() => {
+    const fetchCars = async () => {
+      const carList = await getAllCars();
+      setCars(carList);
+    };
+    fetchCars();
+  }, []);
+
+
   return (
     <div>
-      <h1>Main Container</h1>
+      <Route path='/cars'>
+        <Cars cars={cars} />
+      </Route>
     </div>
   )
 }
---
+++ client/src/screens/Cars/Cars.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+export default function Cars(props) {
+  return (
+    <div>
+      <h3>Cars</h3>
+      {props.cars.map((car) => (
+        <div key={car.id}>
+          <Link to={`/cars/${car.id}`}>
+            <p>{car.make}</p>
+          </Link>
+          <Link to={`/cars/${car.id}/edit`}>
+            <button>Edit</button>
+          </Link>
+          {/* <button onClick={() => props.handleCarDelete(car.id)}>
+            Delete
+          </button> */}
+        </div>
+      ))}
+      <Link to='/cars/new'>
+        <button>create</button>
+      </Link>
+    </div>
+  );
+}
